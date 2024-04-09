7bcaaa2dc695a6254c27f17a1c5be7ef09534f7e
Sungava-collage <sungava24@gmail.com>
2024-04-09 07:31:59 +0545

    KKKK

---
+++ java/WritetoReadFromFile.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Scanner;
+public class WritetoReadFromFile
+{
+    public static void main(String[] args) {
+        try
+        {
+            
+        }
+    }
+}
