d11b863cd0877382ce10286823b99a8340fea154
Sungava-collage <sungava24@gmail.com>
2024-04-14 08:39:47 +0545

    Hello Kashi Don

--- ValueOfExample.java
+++ ValueOfExample.java
@@ -1,16 +1,16 @@
 public class ValueOfExample {
     public static void main(String[] args) {
-        System.out.println("Demonstrating valueOf(int i) methord");
+        System.out.println("Demonstrating valueOf(int i) method");
         Integer i = Integer.valueOf(50);
         Double d = Double.valueOf(9.36);
         System.out.println("i = " + i);
         System.out.println("d = "+d);
-        System.out.println("Demonstrating valueOf(String i) methord");
+        System.out.println("Demonstrating valueOf(String i) method");
         Integer n = Integer.valueOf("333");
         Integer m = Integer.valueOf("-255");
         System.out.println("n = " + n);
         System.out.println("m = " + m);
-        System.out.println("demonstrting (String s, int red x) methord");
+        System.out.println("demonstrating (String s, int red x) method");
         Integer x = Integer.valueOf("333", 8);
         Integer y = Integer.valueOf("-255", 16);
         Long l = Long.valueOf("51688245",16);
---
+++ java/unit9/StackOperation.java
@@ -0,0 +1,58 @@
+package unit9;
+import java.util.*;
+
+import java.util.Stack;
+
+public class StackOperation {
+    static void stack_push(Stack<Integer>stack)
+    {
+        for(int i = 0; i<5;i++)
+        {
+            stack.push(i);
+        }
+
+    }
+    
+    static void stack_pop(Stack<Integer>stack)
+    {
+        System.out.println("POP Operion: ");
+        for(int i = 0; i<5 ; i++)
+        {
+            Integer y= (Integer) stack.pop();
+            System.out.println("Y ="+y);
+        }
+    }
+    static void stack_peek(Stack<Integer>stack)
+    {
+        Integer element = (Integer) stack.peek();
+        System.out.println("Element on stack top :"+element);
+    }
+    static void Stack_search(Stack<Integer> stack, int element)
+    {
+        Integer pos=(Integer) stack.search(element);
+        if(pos ==-1)
+        {
+            System.out.println("Element not found");
+
+        }
+        else
+        {
+            System.out.println("Element found  position: "+pos);
+
+        }
+    }
+    public static void main(String[] args) {
+        
+        Stack<Integer> stack = new Stack<Integer>();
+        stack_push(stack);
+        stack_pop(stack);
+        stack_push(stack);
+        stack_peek(stack);
+        Stack_search(stack,2);
+        Stack_search(stack,6);
+
+
+    }
+}
+
+
