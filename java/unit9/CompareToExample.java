02327062d85d9ac1dace99c120053f2268f97f13
Sungava-collage <sungava24@gmail.com>
2024-04-14 07:13:34 +0545

    Kashi

---
+++ java/unit9/CompareToExample.java
@@ -0,0 +1,11 @@
+package unit9;
+public class CompareToExample
+{
+    public static void main(String args[])
+    {
+        Integer i=new Integer(10);
+        System.out.println(i.compareTo(7));
+        System.out.println(i.compareTo(11));
+        System.out.println(i.compareTo(10));
+    }
+}
