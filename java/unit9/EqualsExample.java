0e322027e658e270b6fb8100409a57c0aab73778
Sungava-collage <sungava24@gmail.com>
2024-04-14 07:16:42 +0545

    kashi Where are you

--- java/unit9/EqualsExample.java
+++ java/unit9/EqualsExample.java
@@ -1,5 +1,14 @@
 package unit9;
 
 public class EqualsExample {
-    
+    public static void main(String[] args) {
+        Short s = new Short("15");
+        Short x= 10;
+        Integer y= 15;
+        Short z = 15;
+        System.out.println(s.equals(z));
+        System.out.println(s.equals(y));
+        System.out.println(s.equals(z));
+
+    }
 }
