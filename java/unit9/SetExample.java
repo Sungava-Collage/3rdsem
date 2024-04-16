39ebbdc92f1baa523586aff25402b0e869f6e761
Sungava-collage <sungava24@gmail.com>
2024-04-16 07:16:28 +0545

    Kashi 3

---
+++ java/unit9/SetExample.java
@@ -0,0 +1,13 @@
+package unit9;
+import java.util.*;
+public class SetExample {
+    public static void main(String[] args) {
+        Set<String> s = new HashSet<String>();
+        s.add("Kashi");
+        s.add("Anish");
+        s.add("Kapil");
+        s.add("Ram");
+        System.out.println(s);
+
+    }
+}
