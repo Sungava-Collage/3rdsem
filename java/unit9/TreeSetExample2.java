0d526c8d55160370d2565b2e592818cc8e743d4b
Sungava-collage <sungava24@gmail.com>
2024-04-16 07:59:03 +0545

    City name in order using Tree ser

---
+++ java/unit9/TreeSetExample2.java
@@ -0,0 +1,26 @@
+package unit9;
+import java.util.*;
+
+public class TreeSetExample2 {
+public static void main(String[] args) {
+    
+        TreeSet<String> s = new TreeSet<>();
+        Scanner sc = new Scanner(System.in);
+        int i,n;
+        String CityName;
+        System.out.println("How many Citys ");    
+        n=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            System.out.println("Enter City Name ");
+            CityName=sc.next();
+            s.add(CityName);
+        }
+        System.out.println("Citys in Sorted Order ");
+        Iterator<String> itr=s.iterator();
+        while(itr.hasNext())
+
+        System.out.println(itr.next());
+    }
+    }
+
