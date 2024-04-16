47b4040a12f2b35307424bb2b93fe6c9f93707fa
Sungava-collage <sungava24@gmail.com>
2024-04-16 08:38:01 +0545

    student of 1

---
+++ java/Unit10/Student.java
@@ -0,0 +1,17 @@
+package Unit10;
+
+/**
+ * Student
+ */
+public class Student {
+
+    int rollno;
+    String name;
+    int age;
+    Student(int rollno,String name, int age)
+    {
+        this.rollno=rollno;
+        this.name=name;
+        this.age=age;
+    }
+}
