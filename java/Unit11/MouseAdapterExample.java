50771f6e1a3e274b81456d5b4d00b7b5113c2408
Sungava-collage <sungava24@gmail.com>
2024-05-29 06:31:43 +0545

    It dont work

---
+++ java/Unit11/MouseAdapterExample.java
@@ -0,0 +1,30 @@
+package Unit11;
+
+import java.awt.*; 
+import java.awt.event.*;
+
+import javax.swing.JFrame;
+
+public class MouseAdapterExample extends MouseAdapter {
+
+    Frame f;
+    MouseAdapterExample(){
+        f = new JFrame("Mouse Adapter");
+        f.setSize(400,500);
+        f.setLayout(null);
+        f.setVisible(true);
+        f.addMouseListener(this);
+        
+    }
+    public void MouseClicked(MouseEvent e)
+    {
+        Graphics g = f.getGraphics();
+        g.setColor(Color.CYAN);
+        g.fillOval(e.getX(), e.getY(), 30, 30);
+
+    }
+    public static void main(String[] args) {
+        new MouseAdapterExample();
+    }
+    
+}
